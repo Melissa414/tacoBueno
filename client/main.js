f28bff4906f36596abb6559f69f45d40e3b196f2
Melissa414 <Missy41491@gmail.com>
2018-04-24 20:43:10 -0400

    fixed sticky footer, working on API

--- client/main.js
+++ client/main.js
@@ -1,3 +1,41 @@
-  $(document).ready(function() {
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://www.yelp.com/", false);
+// xhr.send();
 
-  });
+// console.log(xhr.status);
+// console.log(xhr.statusText);
+
+var locations;
+var food;
+var ratings;
+
+function newSearch() {
+  $(".searchBar").empty();
+    $("#searchBtn").on('click', function (){
+          var inputVal = $(".searchBar").val();
+            newSearch(inputVal);
+            console.log("working");
+
+
+return false;
+  })
+};
+
+function getResponse() {
+    $("#one").empty();
+
+var apiKey = "A6GkgtsWRapq-05zuRUZJk3-H3CmXGg6cMVi2sC2KvQ2UuWHsFZFGEJeL_wvBn3ylhB3oPmTY7hOtTq0j05MlJWvmQrxUWGjHtaFLCu_BuvA1fM_RrM6iHfJr8TfWnYx";
+
+$.ajax({
+            url: apiKey,
+            method: 'GET'
+        })
+        .done(function(response) {
+            // console.log(response);
+
+           var results = response.data;
+    })
+};
+
+// var json = JSON.parse(apiKey);
+// console.log(json);
